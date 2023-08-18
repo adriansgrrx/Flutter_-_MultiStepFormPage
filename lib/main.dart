63e623e464dbedffcaa8d7742933b0264298cf6a
Adrian R. Esguerra <i.am.adriansgrrx@gmail.com>
2023-08-18 14:05:23 +0800

    --

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,6 @@ import 'package:smart_ledger_ai/GettingStarted/6_Bank.dart';
 import 'package:smart_ledger_ai/GettingStarted/7_BranchDivision.dart';
 import 'package:smart_ledger_ai/Masterlist/1_CustomerImportImage.dart';
 import 'package:smart_ledger_ai/Dashboard/MainDashboard.dart';
-import 'package:smart_ledger_ai/PurchasesAPV/1_PurchasesAPV.dart';
-import 'package:smart_ledger_ai/SettingsPreferences/DefaultBackup1.dart';
-import 'package:smart_ledger_ai/SettingsPreferences/DefaultBackup.dart';
-import 'package:smart_ledger_ai/SettingsPreferences/test_test.dart';
 import 'package:smart_ledger_ai/SettingsPreferences/1_SettingsPreferences.dart';
 
 import 'Registration/LoginScreen.dart';
@@ -25,7 +21,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MultiStepFormPage(),
+      home: SettingsPreferences(),
     );
   }
 }
