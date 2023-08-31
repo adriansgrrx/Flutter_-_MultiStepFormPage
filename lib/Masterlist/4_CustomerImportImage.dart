6c6858a45c6eb6c41b7546d13d6bae3459af9302
Adrian R. Esguerra <i.am.adriansgrrx@gmail.com>
2023-08-31 11:02:55 +0800

    Customer Information table structure done.

--- lib/Masterlist/4_CustomerImportImage.dart
+++ lib/Masterlist/4_CustomerImportImage.dart
@@ -6,7 +6,8 @@ class CustomerImportImagePage extends StatefulWidget {
   const CustomerImportImagePage({super.key});
 
   @override
-  State<CustomerImportImagePage> createState() => _CustomerImportImagePageState();
+  State<CustomerImportImagePage> createState() =>
+      _CustomerImportImagePageState();
 }
 
 class _CustomerImportImagePageState extends State<CustomerImportImagePage> {
@@ -18,39 +19,109 @@ class _CustomerImportImagePageState extends State<CustomerImportImagePage> {
           backgroundColor: const Color(0xFFFFFBF2),
           appBar: AppBar(
             iconTheme: const IconThemeData(color: Colors.black),
-            title: Row(
-              children: [
-                Text(
-                  'Box',
-                  style: GoogleFonts.plusJakartaSans(
-                    color: const Color(0xFF040404),
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'ERP',
-                  style: GoogleFonts.plusJakartaSans(
-                    color: const Color(0xFF2C3A76),
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            // leading: Padding(
-            //   padding: const EdgeInsets.all(4.0),
-            //   child: GestureDetector(
-            //     onTap: () {},
-            //     child: const Icon(
-            //       Icons.menu,
-            //       color: Color(0xFF040404),
-            //     ),
-            //   ),
-            // ),
             actions: <Widget>[
               Row(
                 children: [
+                  Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 45.0),
+                        child: Container(
+                          width: 130,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF2C3A76),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: DropdownButton<String>(
+                            onChanged: (String? newValue) {
+                              // Handle dropdown value change
+                            },
+                            items: <String>[
+                              'Option 1',
+                              'Option 2',
+                              'Option 3',
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            style: GoogleFonts.plusJakartaSans(
+                              color: const Color(0xFF010A10),
+                              fontSize: 10.0,
+                            ),
+                            underline: Container(
+                              color: Colors.transparent,
+                            ),
+                            icon: Transform.translate(
+                              offset: const Offset(
+                                  -2, -2), // Adjust the Y offset as needed
+                              child: const Icon(
+                                Icons.arrow_drop_down,
+                                color: Colors.white,
+                              ),
+                            ),
+                            hint: Padding(
+                              padding: const EdgeInsets.only(left: 10.0),
+                              child: Text(
+                                'Categories',
+                                style: GoogleFonts.plusJakartaSans(
+                                    color: Colors.white,
+                                    fontSize: 10.0,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 10.0,
+                        top: 0.0, // Adjust this value to control overlap amount
+                        child: Container(
+                          width: 80,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: const BorderRadius.only(
+                              topRight: Radius.circular(10.0),
+                              bottomLeft: Radius.circular(10.0),
+                            ),
+                            border: Border.all(
+                              color: Colors.black, // Set the border color
+                              width: 0.1, // Set the border width
+                            ),
+                          ),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: 5.0,
+                              ),
+                              Expanded(
+                                child: TextField(
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 10.0),
+                                  decoration: InputDecoration(
+                                      // hintText: 'Search',
+                                      hintStyle: TextStyle(
+                                          color: Colors.black, fontSize: 10.0),
+                                      border: InputBorder.none,
+                                      contentPadding:
+                                          EdgeInsets.symmetric(vertical: 13.0)),
+                                ),
+                              ),
+                              Icon(
+                                Icons.search,
+                                color: Colors.black,
+                                size: 20.0,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                   Padding(
                     padding: const EdgeInsets.only(right: 8.0),
                     child: GestureDetector(
@@ -156,15 +227,10 @@ class _CustomerImportImagePageState extends State<CustomerImportImagePage> {
                     top: 120.0,
                     left: 240.0,
                   ),
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) => const LoginScreen()));
-                    },
+                  child: GestureDetector(
+                    onTap: () {},
                     child: Container(
-                      width: 120,
+                      width: 133,
                       height: 40,
                       decoration: BoxDecoration(
                         color:
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ import 'package:smart_ledger_ai/BankingReconcile/2_BankReconcile.dart';
 import 'package:smart_ledger_ai/GettingStarted/5_Supplier.dart';
 import 'package:smart_ledger_ai/GettingStarted/6_Bank.dart';
 import 'package:smart_ledger_ai/GettingStarted/7_BranchDivision.dart';
-import 'package:smart_ledger_ai/Masterlist/1_CustomerImportImage.dart';
+import 'package:smart_ledger_ai/Masterlist/1_CustomerInformation.dart';
+import 'package:smart_ledger_ai/Masterlist/4_CustomerImportImage.dart';
 import 'package:smart_ledger_ai/Dashboard/MainDashboard.dart';
 import 'package:smart_ledger_ai/PurchasesAPV/1_PurchasesAPV.dart';
 import 'package:smart_ledger_ai/SettingsPreferences/1_SettingsPreferences.dart';
@@ -24,7 +25,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BankReconcile(),
+      home: CustomerInfoDashboard(),
     );
   }
 }
